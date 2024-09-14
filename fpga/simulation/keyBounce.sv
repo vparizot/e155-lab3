d2c9bf6b2bd78cd74eda8dfd6f4b18adac1b1672
Victoria <vparizot@g.hmc.edu>
2024-09-14 15:32:43 -0700

    added debouncer

--- fpga/simulation/keyBounce.sv
+++ fpga/simulation/keyBounce.sv
@@ -1,7 +1,29 @@
 
 module keyBounce(
-	input logic [3:0] decoded key
+	input logic clk, reset,
+	input logic [3:0] keyDecoded,
+	input logic keyPressed,
+	output logic [3:0] keyDebounced
 );
+
+
+//define counter variable
+logic [10:0] counter = 0;
+
+always_ff @(posedge clk, posedge reset)
+	begin
+		if (counter > 11'b11111111110) counter <= 6; // reset counter incase long button press
+		else if (counter > 5) keyDebounced = keyDecoded; // if threshold is met, output KeyDecoded
+	
+		if (keyPressed) counter <= counter + 1; // if key is pressed, add to counter
+			
+		else counter <= 0; // reset counter if key is not pressed
+			
+
+
+	end
+
+
 
 
 endmodule
